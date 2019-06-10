8ac9f08a81b0de420681ef2a045c5fb3c7b29d93
tahentx <hendricks.ta@gmail.com>
2019-06-10 15:22:45 -0700

    calculate expected value of the cost of 5 outages in the future

--- warranty.py
+++ warranty.py
@@ -1,4 +1,10 @@
-import pydecisions as pyd
-
-a = pyd.fin()
-print(a.fv(0.10, 9, 300, 400))
+import numpy as np
+a = [28435,43339,31211,10045,15932]
+n = len(a)
+def get_EV(a,n):
+    prob = 1 / n
+    sum = 0
+    for i in range(0,n):
+        sum += (a[i] * prob)
+        return float(sum)
+get_EV(a,n)
